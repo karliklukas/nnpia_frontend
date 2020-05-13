e2b9218456ddd746996f2da4f08bfcdb93928f14
karliklukas <43926969+karliklukas@users.noreply.github.com>
2020-05-13 18:34:17 +0200

    heroku

--- src/service/CartService.js
+++ src/service/CartService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AuthService from './AuthService';
 
-const USER_API_BASE_URL = 'http://localhost:8080/api/cart';
+const USER_API_BASE_URL = 'https://nnpia-karlik.herokuapp.com/api/cart';
 
 class CartService {
 
